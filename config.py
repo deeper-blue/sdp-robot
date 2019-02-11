32c75364b1d94992410c126a948a8da2d9489306
Jeremy Scott <jeremyascott17@gmail.com>
2019-02-11 15:12:24 +0000

    Add a configuration file

---
+++ config.py
@@ -0,0 +1,28 @@
+# This file is used to hold the different measurements of the chess board, which will be used in the implementation of the robot.
+# Allows us to abstract from the main program.
+# Author(s):
+#    Jeremy Scott
+
+# The length of one side of a square on the chess board in cm
+square_length = 3.8
+
+# Total length of row in cm (start of board to end of buffer)
+row_length = 58
+
+# Total length of the columns in cm (top platform)
+column_length = 50
+
+# The length from the end of the last row to the start of the buffer in cm
+buffer_offset = 12
+
+# Height of the board in cm
+board_height = 2.4
+
+# Tallest piece in cm
+tallest_piece = 7.9
+
+# Wheel circumference in cm
+wheel_circ = 12.8
+
+# Grabber height in cm
+grabber_height = 34

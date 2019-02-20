e2f42b84c5d3a0d5f43e20498071185d8d429eff
Stewart Wilson <stewart.wilson98@gmail.com>
2019-02-20 18:55:47 +0000

    Add hli test files

---
+++ hli_test.py
@@ -0,0 +1,36 @@
+# HLI implementation test.
+#
+# Author(s):
+#   Stewart Wilson
+
+from high_level import config
+from high_level import hli_implementation
+
+def run():
+
+    # Instantiate hli
+    hli = hli_implementation.High_Level_Interface()
+
+    # Run experiment 1: Moving piece
+    cellA = ('B',2)
+    cellB = ('D',4)
+
+    hli.move_piece(cellA, cellB)
+    ##### End experiment 1 #####
+    print('\n')
+    # Run experiment 2: Taking piece
+    cellB = ('D',4)
+    cellC = ('D',6)
+    piece = "white_king"
+
+    hli.take_piece(cellB, cellC, "white_king")
+    ##### End experiment 2 #####
+    print('\n')
+    # Run experiment 3: Castling
+    cellA = ('A',5)
+    cellB = ('A',1)
+    cellC = ('A',7)
+    cellD = ('A',6)
+
+    hli.perform_castling_at(cellA, cellB, cellC, cellD)
+    ##### End experiment 2 #####
---
+++ test.py
@@ -0,0 +1,8 @@
+# Python test script so packages are properly set up.
+#
+# Author(s):
+#  Stewart Wilson
+
+from test.demo_2 import hli_test
+
+hli_test.run()

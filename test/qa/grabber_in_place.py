e183c79110150faa55b35e1f253e57b5ba1320a2
Filip Smola <smola.filip@hotmail.com>
2019-03-06 10:12:04 +0000

    Add grabber in-place QA script

---
+++ test/qa/grabber_in_place.py
@@ -0,0 +1,21 @@
+# Quantitative analysis of Grabber: picking a piece up and putting it back down in the same square 10 times.
+# Performed at A1.
+#
+# Author(s):
+#   Filip Smola
+
+import time
+from test.demo_2 import hli_prepare as env
+
+# Reset and move to up
+env.hli.reset()
+env.pl.centre()
+env.hli.go_to_cell(('A',1))
+
+# Repeat experiment
+print("Starting Grabber in-place QA")
+for i in range(0,10):
+    time.sleep(2)
+    env.hli.pick_up()
+    env.hli.put_down()
+    input("Waiting for Enter...")

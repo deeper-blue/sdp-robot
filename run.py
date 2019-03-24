dcc2de1f6714ea9e16f49b2349d6106829da5e23
Filip Smola <smola.filip@hotmail.com>
2019-03-24 13:25:21 +0000

    Update run and environment preparation scripts to use new HLI constructor

--- run.py
+++ run.py
@@ -5,15 +5,17 @@
 
 from comms import server
 from high_level import hli_implementation
-from low_level import arch, platform, grabber
+from low_level import arch, platform, grabber, pickup, button
 
 # Instantiate LLI
 ar = arch.Arch()
 pl = platform.Platform()
 gr = grabber.grabber
+pi = pickup.Pickup()
+bu = button.Button()
 
 # Instantiate HLI
-hli = hli_implementation.High_Level_Interface(('A',1), ar, pl, gr)
+hli = hli_implementation.High_Level_Interface(('A',1), ar, pl, gr, pi, bu)
 
 # Instantiate server
 ser = server.Server(hli)
--- test/demo_2/hli_prepare.py
+++ test/demo_2/hli_prepare.py
@@ -4,12 +4,14 @@
 #   Filip Smola
 
 from high_level import hli_implementation
-from low_level import arch, platform, grabber
+from low_level import arch, platform, grabber, pickup, button
 
 # Instantiate LLI
 ar = arch.Arch()
 pl = platform.Platform()
 gr = grabber.grabber
+pi = pickup.Pickup()
+bu = button.Button()
 
 # Instantiate hli
-hli = hli_implementation.High_Level_Interface(('L',1), ar, pl, gr)
+hli = hli_implementation.High_Level_Interface(('L',1), ar, pl, gr, pi, bu)

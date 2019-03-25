224de9d32beaae3cd1757754006bfbc505170504
Filip Smola <smola.filip@hotmail.com>
2019-03-25 16:06:19 +0000

    Fix button file name and ev3 interaction

--- low_level/button.py
+++ low_level/button.py
@@ -6,6 +6,9 @@
 import ev3dev.ev3 as ev3
 
 class Button:
+    # EV3 button interface
+    button = ev3.Button()
+
     # True iff the resume button is pressed
     def pressed(self):
-        return ev3.Button.enter
+        return self.button.enter
